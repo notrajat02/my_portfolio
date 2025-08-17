2fd28d5adf7852caf56c5d8bb6847cfa7d031455
Rajat Raj <rajatraj7068@gmail.com>
2025-08-17 13:31:11 +0530

    deploying my portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+
+// next.config.mjs
+
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Enables static export
+  images: { unoptimized: true }, // GitHub Pages doesn't handle next/image optimization
+  basePath: '/my_portfolio', // repo name
+  assetPrefix: isProd ? '/my_portfolio/' : '',
+};
 
 export default nextConfig;
