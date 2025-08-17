34a626eba18cdc0dd3450842d68990990e497ab9
Rajat Raj <rajatraj7068@gmail.com>
2025-08-17 14:02:40 +0530

    test

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
   <header className="bg-gray-900 border-b border-gray-700 shadow-md h-20">
       <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
         <div className="flex items-center">
-          <span className="font-bold text-3xl text-white italic font-serif bg-black bg-opacity-60 px-4 py-2 rounded">Creat</span>
+          <span className="font-bold text-3xl text-white italic font-serif bg-black bg-opacity-60 px-4 py-2 rounded">CreatSpace</span>
         </div>
         {/* Desktop Navigation */}
         <nav className="hidden md:flex">
