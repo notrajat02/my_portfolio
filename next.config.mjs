55a3b5f8b64e6d46b73f0b683ce953fbf01e50c5
Rajat Raj <rajatraj7068@gmail.com>
2025-08-17 13:46:18 +0530

    Changes made

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true }
+};
 
 export default nextConfig;
