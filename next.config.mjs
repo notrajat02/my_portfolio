53b688ba39e433c08ed738e3b6433e41b5436164
Rajat Raj <rajatraj7068@gmail.com>
2025-08-17 13:45:14 +0530

    Deploying to netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
-
-// next.config.mjs
-
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Enables static export
-  images: { unoptimized: true }, // GitHub Pages doesn't handle next/image optimization
-  basePath: '/my_portfolio', // repo name
-  assetPrefix: isProd ? '/my_portfolio/' : '',
-};
+const nextConfig = {};
 
 export default nextConfig;
